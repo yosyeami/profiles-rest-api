d300755d4b6d4c86fb169dfade03ceba1f6f9eac
yosyeami <you@example.com>
2021-11-19 16:29:47 +0300

    Added profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
 
+router = DefaultRouter()
+router.register('profile', views.UserProfileViewSet)
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
